6601c541644bbedff85829253d7cbb9308473312
zouzou6900 <36742278+zouzou6900@users.noreply.github.com>
2024-03-12 01:47:17 +0100

    add router dashboard

--- src/app/pages/admin/content/content.component.ts
+++ src/app/pages/admin/content/content.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { interval } from 'rxjs';
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
-export class ContentComponent  {
-  dateActuelle: Date = new Date();
-  title="Dashboard";
-  constructor(private router: Router) { }
-
- 
-  
-}
+export class ContentComponent {}
