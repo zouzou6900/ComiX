8dc30fd0e5750914892329bd3aeca7562b9de1e5
Dmokrite1 <hg_greg@proton.me>
2024-03-19 16:19:28 +0100

    Add aboutComponentHtml, aboutComponentCss + contactComponentHtml, contactComponentCss + adding stuff in homeComponentHtml, homeComponentCss

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
