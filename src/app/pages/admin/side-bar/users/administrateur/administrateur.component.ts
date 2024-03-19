930f4067c42faf188cf8f3d32316e9f32eda0f81
zouzou6900 <36742278+zouzou6900@users.noreply.github.com>
2024-03-19 22:44:17 +0100

    tableau ts

--- src/app/pages/admin/side-bar/users/administrateur/administrateur.component.ts
+++ src/app/pages/admin/side-bar/users/administrateur/administrateur.component.ts
@@ -2,90 +2,44 @@ import { Component } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { UserListService } from '../../../../../services/user-list.service';
+import { ListUser } from '../../../../../interfaces/list-user';
 
-export interface PeriodicElement {
-  name: string;
-  id: number;
-  email: string;
-  role: string;
-  naissance: Date;
-  register: Date;
-}
+// export interface PeriodicElement {
+//   name: string;
+//   id: number;
+//   email: string;
+//   role: string;
+//   naissance: Date;
+//   register: Date;
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    id: 1,
-    name: 'zouzou',
-    email: '',
-    role: 'H',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 2,
-    name: 'Helium',
-    email: '',
-    role: 'He',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 3,
-    name: 'Lithium',
-    email: '6.941',
-    role: 'Li',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 4,
-    name: 'Beryllium',
-    email: '9.0122',
-    role: 'Be',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 5,
-    name: 'Boron',
-    email: '10.811',
-    role: 'B',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 6,
-    name: 'Carbon',
-    email: '12.0107',
-    role: 'C',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 7,
-    name: 'Nitrogen',
-    email: '14.0067',
-    role: 'N',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 9,
-    name: 'Fluorine',
-    email: '18.9984',
-    role: 'F',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-  {
-    id: 10,
-    name: 'Neon',
-    email: '20.1797',
-    role: 'Ne',
-    naissance: new Date('1990-04-29'),
-    register: new Date('1990-04-28'),
-  },
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {
+//     id: 1,
+//     name: 'zouzou',
+//     email: '',
+//     role: 'H',
+//     naissance: new Date('1990-04-29'),
+//     register: new Date('1990-04-28'),
+//   },
+//   {
+//     id: 2,
+//     name: 'Helium',
+//     email: '',
+//     role: 'He',
+//     naissance: new Date('1990-04-29'),
+//     register: new Date('1990-04-28'),
+//   },
+//   {
+//     id: 3,
+//     name: 'Lithium',
+//     email: '6.941',
+//     role: 'Li',
+//     naissance: new Date('1990-04-29'),
+//     register: new Date('1990-04-28'),
+//   },
+// ];
 @Component({
   selector: 'app-administrateur',
   standalone: true,
@@ -101,11 +55,34 @@ export class AdministrateurComponent {
     'role',
     'naissance',
     'register',
+    'action'
   ];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  
+
+  dataSource: ListUser[] = [];
+  dataSources: any;
+
+  constructor(private userListService: UserListService) {}
+
+  ngOnInit(): void {
+    this.userListService.getAllUsers().subscribe(
+      (data: any) => {
+        this.dataSource = data.users;
+      },
+      (error) => {
+        console.error(
+          'Erreur lors de la récupération des utilisateurs:',
+          error
+        );
+      }
+    );
+     let dataSources = new MatTableDataSource(this.dataSource);
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSources.filter = filterValue.trim().toLowerCase();
   }
+
+  
 }
